3f6afdffdb6b6baaf1ab54ff392dff79aeac4914
ekh <ekh@d076a09b-9ea0-46ec-bd2f-d44f26dbd69a>
2010-07-10 22:20:31 +0000

    Added Setup Project

--- BACnet Server/demo/server/main.c
+++ BACnet Server/demo/server/main.c
@@ -1,7 +1,7 @@
 /**************************************************************************
 *
 * Copyright (C) 2006 Steve Karg <skarg@users.sourceforge.net>
-*
+* 
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
 * "Software"), to deal in the Software without restriction, including
